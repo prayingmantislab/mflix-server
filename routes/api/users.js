0161f87a1bd3dc60da0cda16193a44871f024a1b
Asi Meir <45923568+prayingmantislab@users.noreply.github.com>
2022-09-11 17:39:20 +0300

    user registeration

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,12 +7,57 @@ const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 
+const User = require('../../models/User');
 // @route   Post api/users
 // @desc    Register user
 // @access  Public
-router.post('/', (req, res) => {
-  console.log(req.body);
-  res.send('User route');
+router.post(
+  '/',
+  [
+    check('username', 'Please include a valid username capital or small letters only minimum 8 characters').isLength({ min: 8 })
+    .matches(/^[A-Za-z0-9 .,'!&]+$/)
+    .not()
+    .isEmpty(),
+    check('password', 'Please enter a password with 6 or more characters, must include capital letters, small letters,numbers and special characters ').isLength({ min: 6 }).matches(/^[A-Za-z0-9 .,'!&]+$/)
+  ],
+   async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { username, password } = req.body;
+debugger
+    try {
+      // See if user exists
+        
+      let user = await User.findOne({ username });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      user = new User({
+        username,
+        password
+      });
+
+
+      // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send('User registered');
+      // Return jsonwebtoken
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
 });
   
 
