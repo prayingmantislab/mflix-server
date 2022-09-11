da32013b03f2e24318b23d9c1a75ec5157e8f217
Asi Meir <45923568+prayingmantislab@users.noreply.github.com>
2022-09-11 18:14:08 +0300

    auth user

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -6,26 +6,20 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 const { check, validationResult } = require('express-validator');
 
+const User = require('../../models/User');
+
 // @route   GET api/auth
 // @desc    Test route
 // @access  Public
-
-router.get('/', (req, res) => res.send('Auth route'));
-
-// const User = require('../../models/User');
-
-// // @route Get api/auth
-// // @desc Test route
-// // @access Public
-// router.get('/', auth, async (req, res) => {
-//   try {
-//     const user = await User.findById(req.user.id).select('-password');
-//     res.json(user);
-//   } catch (err) {
-//     console.error(err.message);
-//     res.status(500).send('Server Eror');
-//   }
-// });
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Eror');
+  }
+});
 
 // // @route   post api/auth
 // // @desc    Authenticate user & get token
